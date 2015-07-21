4a2d15c2c51ada12e7d92a28e5621d83e32c9930
Mari Kaneshima <mari2daniel@yahoo.com>
2015-07-21 14:42:58 -0700

    Added new photos to test ChosenRecipeViewController

---
+++ TemplateProject/Models/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  TemplateProject
+//
+//  Created by Mari Kaneshima on 7/21/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+   
+}
