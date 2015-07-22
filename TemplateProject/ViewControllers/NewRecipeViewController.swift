6562649cae3c332d47f14d6184ce9d7679b40732
Mari Kaneshima <mari2daniel@yahoo.com>
2015-07-21 17:14:36 -0700

    Added NewRecipeViewController.swift

---
+++ TemplateProject/ViewControllers/NewRecipeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewRecipeViewController.swift
+//  TemplateProject
+//
+//  Created by Mari Kaneshima on 7/21/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+import Parse
+import Bond
+
+class NewRecipeViewController: UIViewController
+{
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
