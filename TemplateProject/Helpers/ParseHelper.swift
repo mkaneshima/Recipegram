c54b2d755e23e546898ff2dd267f140b408a99a7
Mari Kaneshima <mari2daniel@yahoo.com>
2015-07-26 19:18:31 -0700

    Modified the ParseHelper

--- TemplateProject/Helpers/ParseHelper.swift
+++ TemplateProject/Helpers/ParseHelper.swift
@@ -99,6 +99,133 @@ class ParseHelper
         
         query.findObjectsInBackgroundWithBlock(completionBlock)
     }
+    
+    // MARK: Flagging
+    static func flagPost(user: PFUser, recipe: Recipe)
+    {
+        let flagObject = PFObject(className: ParseFlaggedContentClass)
+        flagObject.setObject(user, forKey: ParseFlaggedContentFromUser)
+        flagObject.setObject(recipe, forKey: ParseFlaggedContentToRecipe)
+        
+        let ACL = PFACL(user: PFUser.currentUser()!)
+        ACL.setPublicReadAccess(true)
+        flagObject.ACL = ACL
+        
+        //TODO: add error handling
+        flagObject.saveInBackgroundWithBlock(ErrorHandling.errorHandlingCallback)
+    }
+    
+    // MARK: Following
+    
+    /**
+    Fetches all users that the provided user is following.
+    
+    :param: user The user who's followees you want to retrive
+    :param: completionBlock The completion block that is called when the query completes
+    */
+    static func getFollowingUsersForUser(user: PFUser, completionBlock: PFArrayResultBlock)
+    {
+        let query = PFQuery(className: ParseFollowClass)
+        
+        query.whereKey(ParseFollowFromUser, equalTo:user)
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+    }
+    
+    /**
+    Establishes a follow relationship between two users.
+    
+    :param: user    The user that is following
+    :param: toUser  The user that is being followed
+    */
+    static func addFollowRelationshipFromUser(user: PFUser, toUser: PFUser)
+    {
+        let followObject = PFObject(className: ParseFollowClass)
+        followObject.setObject(user, forKey: ParseFollowFromUser)
+        followObject.setObject(toUser, forKey: ParseFollowToUser)
+        
+        followObject.saveInBackgroundWithBlock(ErrorHandling.errorHandlingCallback)
+    }
+    
+    /**
+    Deletes a follow relationship between two users.
+    
+    :param: user    The user that is following
+    :param: toUser  The user that is being followed
+    */
+    static func removeFollowRelationshipFromUser(user: PFUser, toUser: PFUser)
+    {
+        let query = PFQuery(className: ParseFollowClass)
+        query.whereKey(ParseFollowFromUser, equalTo:user)
+        query.whereKey(ParseFollowToUser, equalTo: toUser)
+        
+        query.findObjectsInBackgroundWithBlock
+        {
+            (results: [AnyObject]?, error: NSError?) -> Void in
+            if let error = error
+            {
+                ErrorHandling.defaultErrorHandler(error)
+            }
+            
+            let results = results as? [PFObject] ?? []
+            
+            for followRelationship in results
+            {
+                followRelationship.deleteInBackgroundWithBlock(ErrorHandling.errorHandlingCallback)
+            }
+        }
+    }
+    
+    // MARK: Users
+    
+    /**
+    Fetch all users, except the one that's currently signed in.
+    Limits the amount of users returned to 20.
+    
+    :param: completionBlock The completion block that is called when the query completes
+    
+    :returns: The generated PFQuery
+    */
+    static func allUsers(completionBlock:PFArrayResultBlock) -> PFQuery
+    {
+        let query = PFUser.query()!
+        // exclude the current user
+        query.whereKey(ParseHelper.ParseUserUsername,
+            notEqualTo: PFUser.currentUser()!.username!)
+        query.orderByAscending(ParseHelper.ParseUserUsername)
+        query.limit = 20
+        
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+        
+        return query
+    }
+    
+    /**
+    Fetch users who's username matches the provided search term.
+    
+    :param: searchText The text that should be used to search for users
+    :param: completionBlock The completion block that is called when the query completes
+    
+    :returns: The generated PFQuery
+    */
+    static func searchUsers(searchText: String, completionBlock: PFArrayResultBlock) -> PFQuery
+    {
+            /*
+            NOTE: We are using a Regex to allow for a case insensetive compare of usernames.
+            Regex can be slow on large datasets. For large amount of data it's better to store
+            lowercased username in a separate column and perform a regular string compare.
+            */
+            let query = PFUser.query()!.whereKey(ParseHelper.ParseUserUsername, matchesRegex: searchText, modifiers: "i")
+            
+            query.whereKey(ParseHelper.ParseUserUsername, notEqualTo: PFUser.currentUser()!.username!)
+            
+            query.orderByAscending(ParseHelper.ParseUserUsername)
+            query.limit = 20
+            
+            query.findObjectsInBackgroundWithBlock(completionBlock)
+            
+            return query
+    }
+
 }
 extension PFObject : Equatable
 {
