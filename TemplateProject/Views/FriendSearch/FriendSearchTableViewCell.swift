168da24b53ad0a512b7d18f5464574fa22fd2491
Mari Kaneshima <mari2daniel@yahoo.com>
2015-07-26 19:16:20 -0700

    Modified the FriendSearchTableViewCell

--- TemplateProject/Views/FriendSearch/FriendSearchTableViewCell.swift
+++ TemplateProject/Views/FriendSearch/FriendSearchTableViewCell.swift
@@ -11,47 +11,56 @@ import Parse
 
 protocol FriendSearchTableViewCellDelegate: class
 {
-    func cell(cell: FriendSearchTableViewCell, didSelectFollowUser user: PFUser)
+    func cell(cell: FriendSearchTableViewCell, didSelectFollowerUser user: PFUser)
     func cell(cell: FriendSearchTableViewCell, didSelectUnfollowUser user: PFUser)
 }
 
 class FriendSearchTableViewCell: UITableViewCell
 {
-    
     @IBOutlet weak var usernameLabel: UILabel!
-    
     @IBOutlet weak var followButton: UIButton!
-    
-    
     weak var delegate: FriendSearchTableViewCellDelegate?
     
     var user: PFUser?
     {
-        didSet {
+        didSet
+        {
             usernameLabel.text = user?.username
         }
     }
     
-    var canFollow: Bool? = true {
-        didSet {
+    var canFollow: Bool? = true
+    {
+        didSet
+        {
             /*
             Change the state of the follow button based on whether or not
             it is possible to follow a user.
             */
-            if let canFollow = canFollow {
+
+            if let canFollow = canFollow
+            {
                 followButton.selected = !canFollow
             }
         }
     }
     
-    @IBAction func followButtonTapped(sender: AnyObject) {
-        if let canFollow = canFollow where canFollow == true {
-            delegate?.cell(self, didSelectFollowUser: user!)
+    @IBAction func followButtonTapped(sender: AnyObject)
+    {
+        if let canFollow = canFollow where canFollow == true
+        {
+            delegate?.cell(self, didSelectFollowerUser: user!)
             self.canFollow = false
-        } else {
+        }
+        else
+        {
             delegate?.cell(self, didSelectUnfollowUser: user!)
             self.canFollow = true
         }
     }
-    
+
+
+
+
+
 }
