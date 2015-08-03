ab2973256f37094fcc97d6504166b58e18807bf5
Mari Kaneshima <mari2daniel@yahoo.com>
2015-08-02 19:21:25 -0700

    Modifying the TableViewCell

--- TemplateProject/Views/Directions/DirectionsTableViewCell.swift
+++ TemplateProject/Views/Directions/DirectionsTableViewCell.swift
@@ -8,29 +8,39 @@
 
 import UIKit
 
+typealias photoTakingHelperCallBack = UIImage? -> Void
+
 class DirectionsTableViewCell: UITableViewCell
 {
+    // MARK: Directions TextView
     @IBOutlet weak var directionsTextView: UITextView!
+    
+    // MARK: Directions ImageView
     @IBOutlet weak var directionsImageView: UIImageView!
+    
+    // MARK: Directions Camera button
     @IBOutlet weak var directionsCameraButton: UIButton!
     
+    // MARK: PhotoTakingHelper
     var photoTakingHelper: PhotoTakingHelper?
     var directions: [String]?
     
     
-//    @IBAction func directionsCameraButtonPressed(sender: AnyObject)
-//    {
-//        photoTakingHelper = PhotoTakingHelper(
-//    }
-    
-    
+    @IBAction func directionsCameraButtonPressed(sender: AnyObject)
+    {
+//        photoTakingHelper = PhotoTakingHelper(viewController: NewRecipeViewController, callback: photoTakingHelperCallBack)
+//        {
+//            
+//        }
+        
+        
+    }
     
     
     override func awakeFromNib()
     {
         super.awakeFromNib()
         // Initialization code
-//        directionsTextView.scrollEnabled = false
         
     }
 
